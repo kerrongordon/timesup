bccb9c20b88c33ecf09539704a2459056fa404be
kerrongordon <kgpsounds.com@gmail.com>
2018-04-21 01:00:33 -0400

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { NewSchedulePage } from '../new-schedule/new-schedule';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController
+  ) {
 
+  }
+
+  addNewSchedule() {
+    let newScMod = this.modalCtrl.create(NewSchedulePage);
+    return newScMod.present();
   }
 
 }
