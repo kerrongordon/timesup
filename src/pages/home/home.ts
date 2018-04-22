8496767ca76946c06e9f9aa893c7d42530021af8
kerrongordon <kgpsounds.com@gmail.com>
2018-04-21 21:04:33 -0400

    upade code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
 import { NewSchedulePage } from '../new-schedule/new-schedule';
+import { ScheduleProvider, Item } from '../../providers/schedule/schedule';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  data: { date: string; items: Item; }[];
+  color = '#555';
 
   constructor(
     public navCtrl: NavController,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    private scheduleProv: ScheduleProvider
   ) {
 
   }
 
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    return this.scheduleProv.getSchedule().then(data => {
+
+      const groups = data.reduce((groups, item) => {
+        const date = item.date;
+        if (!groups[date]) { groups[date] = []; }
+        groups[date].push(item);
+        return groups;
+      }, {});
+
+      const groupArrays = Object.keys(groups).map((date) => {
+        return {
+          date,
+          items: groups[date]
+        };
+      });
+
+      this.data = groupArrays.sort((a,b) => {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+      });
+
+    });
+  }
+
   addNewSchedule() {
     let newScMod = this.modalCtrl.create(NewSchedulePage);
-    return newScMod.present();
+    newScMod.onDidDismiss(() => this.loadData());
+    newScMod.present();
+  }
+
+  openItem() {
+    console.log('item open');
+  }
+
+  deleteItem() {
+    console.log('item remove');
+  }
+
+  acrhiveItem() {
+    console.log('item archive');
   }
 
 }
--- src/pages/new-schedule/new-schedule.ts
+++ src/pages/new-schedule/new-schedule.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ViewController } from 'ionic-angular';
+import { ViewController, ToastController } from 'ionic-angular';
+import { Md5 } from 'ts-md5/dist/md5';
+import { ScheduleProvider, Item } from '../../providers/schedule/schedule';
 
 @Component({
   selector: 'page-new-schedule',
@@ -7,15 +9,39 @@ import { NavController, NavParams, ViewController } from 'ionic-angular';
 })
 export class NewSchedulePage {
 
+  limTime: string;
+  limDate: string;
+
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public viewCtrl: ViewController
+    public viewCtrl: ViewController,
+    private scheduleProv: ScheduleProvider,
+    private toastCtrl: ToastController
   ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewSchedulePage');
+    this.limitDateTime();
+  }
+
+  addZero(num: number) {
+    if (num.toString().length === 1) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  limitDateTime() {
+    const theDate = new Date();
+    const year = theDate.getUTCFullYear();
+    const month = theDate.getUTCMonth() + 1;
+    const date = theDate.getDate();
+    const monthzero = this.addZero(month);
+    const datezero = this.addZero(date);
+
+    console.log(`${year}-${monthzero}-${datezero}`);
+
+    return this.limDate = `${year}-${monthzero}-${datezero}`;
   }
 
   getWeekDay(date) {
@@ -35,14 +61,59 @@ export class NewSchedulePage {
   }
 
   onCreateSchedule(e) {
-    console.log(e.value);
-    const { date } = e.value;
-    let test = this.getWeekDay(date);
-    console.log(test);
+    if (!e.valid) return;
+    const time = new Date().toString();
+    const hash = Md5.hashStr(time);
+    const data: Item = e.value;
+    this.getWeekDay(data.date);
+
+    if (data.title === '' || data.title === null) {
+      return this.isValid('Please add a Title');
+    }
+
+    if (data.body === '' || data.body === null) {
+      return this.isValid('Please add a Description');
+    }
+
+    if (data.date === '' || data.date === null) {
+      return this.isValid('Please add a Date');
+    }
+
+    if (data.time === '' || data.time === null) {
+      return this.isValid('Please add a Time');
+    }
+
+    if (data.color === '' || data.color === null) {
+      return this.isValid('Please add a Color');
+    }
+
+    const newData = {
+      id: hash,
+      title: data.title,
+      body: data.body,
+      date: data.date,
+      time: data.time,
+      color: data.color,
+      isArchive: false,
+      isDone: false
+    }
+
+    this.scheduleProv.addSchedule(data)
+    return this.dismiss();
   }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  isValid(message: string) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+
+    return toast.present();
   }
 
 }
